bd74f5bfd59a7ff5b6d423b79b5891f68f1c2637
Siebe Jan <siebejan@hackerone.com>
2016-07-11 17:16:24 +0200

    add integration command

---
+++ lib/kender/commands/integration.rb
@@ -0,0 +1,19 @@
+module Kender
+  class Integration < Command
+
+    def available?
+      # do not run if running shamus
+      return false if ENV['VALIDATE_PROJECT']
+      in_gemfile?("rspec") || in_gemfile?("rspec-rails")
+    end
+
+    def command
+      if defined?(ParallelTests)
+        'bundle exec rake parallel:spec[^spec/integration]'
+      else
+        'bundle exec rspec[^spec/integration]'
+      end
+    end
+
+  end
+end
